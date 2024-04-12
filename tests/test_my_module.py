ed9cc6e96930857c1baf8a147a38353b50eeede2
Henock Kinfegabrail <henockgebriel@gmail.com>
2024-04-12 09:33:58 +0300

    unit test modified

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -1,11 +1,25 @@
+import os
 import unittest
-from my_module import add_numbers
+import pandas as pd
 
-class TestMyModule(unittest.TestCase):
-    def test_add_numbers(self):
-      
-        result = add_numbers(2, 3)
-        self.assertEqual(result, 5)  
+class TestData(unittest.TestCase):
+    def setUp(self):
+        # Get the current directory
+        current_dir = os.path.dirname(__file__)
+        # Load the data
+        data_path = os.path.join(current_dir, '..', 'data', 'rating.csv')
+        self.data = pd.read_csv(data_path)
+
+    def test_data_columns(self):
+        # Ensure that the dataset contains the expected columns
+        expected_columns = [
+            'article_id', 'source_id', 'source_name', 'author', 'title',
+            'description', 'url', 'url_to_image', 'published_at', 'content',
+            'category', 'article', 'title_sentiment'
+        ]
+        actual_columns = self.data.columns.tolist()
+        self.assertEqual(actual_columns, expected_columns, 
+                         "Columns in the dataset do not match the expected columns")
 
 if __name__ == '__main__':
     unittest.main()
